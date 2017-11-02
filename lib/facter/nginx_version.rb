07ec197d9b733ddf624780c08071878ced53b046
Mike Smith <mike.smith@puppet.com>
2017-11-02 09:44:44 -0700

    Update nginx_version.rb

--- lib/facter/nginx_version.rb
+++ lib/facter/nginx_version.rb
@@ -1,6 +1,9 @@
 Facter.add(:nginx_version) do
   setcode do
-    if Facter.value('kernel') != 'windows' && Facter.value('operatingsystem') != 'nexus' && (Facter::Util::Resolution.which('nginx') || Facter::Util::Resolution.which('openresty'))
+     Facter.value(:kernel) != 'windows' and Facter.value(:operatingsystem) != 'nexus'
+  end
+  setcode do
+    if (Facter::Util::Resolution.which('nginx') || Facter::Util::Resolution.which('openresty'))
       nginx_version_command = Facter::Util::Resolution.which('nginx') ? 'nginx -v 2>&1' : 'openresty -v 2>&1'
       nginx_version = Facter::Util::Resolution.exec(nginx_version_command)
       %r{nginx version: (nginx|openresty)\/([\w\.]+)}.match(nginx_version)[2]
